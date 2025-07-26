818b3b7572009a98b52e23bd10d46bd6fb0abe68
Karan Singh Rana <karancollegestuffs@gmail.com>
2025-07-26 14:12:39 -0500

    Add files via upload

---
+++ greeter.py
@@ -0,0 +1,13 @@
+first_name = input("What is your first name?\n")
+
+last_name = input("What is your last name?\n")
+
+age = input("Okay, what is your age?\n")
+
+print(
+    f"Your name is {first_name.title()} {last_name.title()} and you are {age} years old.\n")
+
+hobby = input("Tell me one sport you love!\n")
+
+print(
+    f"So your name is {first_name.title()} {last_name.title()}, you are {age} years old and you like playing {hobby.lower()}.")
